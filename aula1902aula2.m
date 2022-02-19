16fa4c6f50cc75cf8ca789492338ca060aa8500b
Junior <72615418+iamjunioru@users.noreply.github.com>
2022-02-19 14:51:25 -0300

    19/02
    correção de alguns erros

--- aula1902aula2.m
+++ aula1902aula2.m
@@ -4,31 +4,44 @@ clc;
 
 pkg load image
 
-i = imread('family2.jpg');
-i = rgb2gray(i);
-i = im2double(i);
+i = imread('letra.jpg');
+ i = rgb2gray(i);
+ i = im2double(i);
 # figure, imshow(i);
 
-g1 = imnoise(i, 'gaussian', 0, 0.001);
+# g1 = imnoise(i, 'gaussian', 0, 0.001);
 # figure, imshow(g1);
 
-g2 = imnoise(i, 'salt & pepper', 0.05);
+#g2 = imnoise(i, 'salt & pepper', 0.05);
 # figure, imshow(g2);
 
-h1 = fspecial('average', 10);
-h2 = imfilter(g1, h1);
+#h1 = fspecial('motion', 45);
+# h2 = imfilter(g1, h1, 'symmetric');
 # figure, imshow(h2);
 
+r1 = fspecial('gaussian', 50, 5);
+r2 = imfilter(i, r1, 'symmetric');
+# figure, imshow(r2);
 
-r = medfilt2(g2, [3 3]);
+
+
+# r = medfilt2(g2, [3 3]);
 # figure, imshow(r);
+g2 = i- r2;
+g3 = i + g2;
+figure, imshow(g3);
+
+
+figure, imshow(g2);
+
+
 
 # imagens 
 figure
 subplot (2,2,1), imshow (i);
 subplot (2,2,2), imshow (g1);
 subplot (2,2,3), imshow (g2);
-subplot (2,2,4), imshow (r);
+subplot (2,2,4), imshow (g3);
 
 # figure
 # subplot (2,2,1), imhist(i);
