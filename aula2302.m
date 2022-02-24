e1b7e1bf8e81b001cfa479e383637dbfe0c97b46
Junior <72615418+iamjunioru@users.noreply.github.com>
2022-02-23 21:35:37 -0300

    23/02

--- aula2302.m
+++ aula2302.m
@@ -4,7 +4,7 @@ clc;
 
 pkg load image
 
-f = imread('koala.jpg');
+f = imread('lol.jpg');
 f = rgb2gray(f);
 f = im2double(f);
 
@@ -21,14 +21,19 @@ g2 = f.*[1-g1];
 
 g3 = imcomplement(f.*[1-g1]);
 
-b = strel('disk', 5, 0);
+b = strel('diamond', 2); # strel('disk', 5, 0);
+
+g4 = imdilate(g1, b);
+
+g5 = imerode(g1, b);
 
 
-# imshow(f); 
+# figure, imshow(f); 
 subplot (3,3,1), imshow (f);
 subplot (3,3,2), imhist (f);
 subplot (3,3,3), imshow (f2);
 subplot (3,3,4), imshow (g1);
 subplot (3,3,5), imshow (g2);
 subplot (3,3,6), imshow (g3);
-subplot (3,3,7), imshow (b);
+subplot (3,3,7), imshow (g4);
+subplot (3,3,8), imshow (g5);
